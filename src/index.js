3e70ad7945b2e959ba890d2945d15b073751e80e
DrBretto <59483898+DrBretto@users.noreply.github.com>
2020-07-16 11:39:44 -0400

    getting closer to being able to start here

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-
 import $ from "jquery";
 import api from "./src/api.js";
 
-
-
 function main() {
+  console.log("literally anything")
   console.log(api.getBookmarks());
   eventListeners();
 }
-
-
-
-
-
 
 
 $(main)
